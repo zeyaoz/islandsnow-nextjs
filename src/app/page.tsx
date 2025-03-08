a62dc8eb8b4ae54387569aad21911a23a7705504
zeyaoz <zeyaoz@hawaii.edu>
2025-03-08 02:26:27 -1000

    Completed VSCode and ESLint setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import './style.css';
+import TopMenu from '@/components/TopMenu';
+import IslandSnowLogo from '@/components/IslandSnowLogo';
+import MiddleMenu from '@/components/MiddleMenu';
+import FullWidthImage from '@/components/FullWidthImage';
+import FooterMenu from '@/components/FooterMenu';
 
-/** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Col>
-      </Row>
+    <Container fluid className="px-0" id="footer">
+      <TopMenu />
+      <IslandSnowLogo />
+      <MiddleMenu />
+      <FullWidthImage />
+      <FooterMenu />
     </Container>
   </main>
 );
